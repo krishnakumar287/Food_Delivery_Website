e1270c8e4eb9765734b372e09fa34627c34bba60
krishna <krishnakuma.s1973@gmail.com>
2025-08-09 21:22:29 +0530

    nav update

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -278,16 +278,7 @@ const Navbar = ({setShowLogin}) => {
                   </svg>
                   <p>My Orders</p>
                 </div>
-                <div className="dropdown-item dropdown-item-animation" onClick={() => {
-                  navigate('/profile');
-                  setProfileDropdownOpen(false);
-                }}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                    <circle cx="12" cy="7" r="4"></circle>
-                  </svg>
-                  <p>My Profile</p>
-                </div>
+                
                 <div className="dropdown-divider"></div>
                 <div className="dropdown-item dropdown-item-animation logout-item" onClick={logout}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
