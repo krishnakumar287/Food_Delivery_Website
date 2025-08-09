0fcca44bd41ec83e7c481258ae8eefab78bae92a
yukesshwaran21 <yukesshwaran6@gmail.com>
2025-08-09 21:45:46 +0530

    added some changes in vit.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASH_PATH || "/Food_Delivery_Website",
   server: {
     proxy: {
       '/api': 'https://food-delivery-website-2-5x0z.onrender.com'
